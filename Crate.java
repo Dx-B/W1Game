a12a4d7193b42b3b72a7bc99552be6ec286a06a1
DxSifi <billyzhangdx@gmail.com>
2022-04-15 13:13:27 -0400

    poopy

---
+++ Crate.java
@@ -0,0 +1,11 @@
+package W1Game;
+
+import java.util.ArrayList;
+
+public class Boxes {
+    private String boxType;
+    String[] boxNames = new String[]{"Wooden","Improvised","Durable","Reinforced",""};
+    public Boxes(String type) {
+        boxType = type;
+    }
+}
